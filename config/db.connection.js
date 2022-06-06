8e06b730547cec015c1237979d99f226a4d98402
brighamlee1 <brighamlee1@gmail.com>
2022-06-06 10:43:33 -0500

    adding jerseys.json

---
+++ config/db.connection.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const connectionStr = process.env.MONGODB_URI;
+
+mongoose.connect(connectionStr);
+
+mongoose.connection.on("connected", () => {
+    console.log(
+        `[${new Date().toLocaleTimeString()}] - MongoDB connected... 🙌 🙌 🙌`
+    );
+});
+
+mongoose.connection.on("error", (error) => {
+    console.log("MongoDB connection error ", error);
+})
+
+mongoose.connection.on("disconnected", () => {
+    console.log("MongoDB disconnected");
+})
--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const cors = require('cors');
 
+// Import JSON files
+const jerseys = require('./jerseys.json')
+
 // Create app object
 const app = express();
+
+require("./config/db.connection");
 
 // Middleware
 app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('hello world');
+})
+
+app.get('/jerseys', (req, res) => {
+    res.json(jerseys);
 })
 
 const PORT = process.env.PORT || 4000;
